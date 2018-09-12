727e60eaea3f48d5d77565bedfb0cdde77ecd064
xljiulang <366193849@qq.com>
2018-09-12 22:18:00 +0800

    更新广告清除条数计数计

--- BaiduAdBlock/scripts/include.js
+++ BaiduAdBlock/scripts/include.js
@@ -1,30 +1,39 @@
 ﻿;
 (function () {
     var delayer = new function () {
-        var hwdn = 0;
+        var _hwdn = 0;
         this.setDelay = function (handler, delay) {
-            clearTimeout(hwdn);
-            hwdn = setTimeout(handler, delay);
+            if (_hwdn) clearTimeout(_hwdn);
+            _hwdn = setTimeout(handler, delay);
         };
     };
 
-    function showClearCount(length) {
-        if (length <= 0) {
-            return;
-        }
+    var counter = new function () {
+        var _count = 0;
 
-        var btn = $('[type="submit"]');
-        if (btn.length > 0) {
-            var count = (btn.data('count') || 0) + length;
-            var title = '已为您清除了' + count + '条广告';
-            var value = '百度一下(' + count + ')';
-            btn.data('count', count).attr('title', title).val(value);
+        this.addCount = function (count) {
+            _count = _count + count;
+            return _count;
+        };
 
-            delayer.setDelay(function () {
-                btn.data('count', 0);
-            }, 1000);
-        }
-    }
+        this.clear = function () {
+            _count = 0;
+        };
+
+        this.showCount = function () {
+            var btn = $('[type="submit"]');
+            if (btn.length) {
+                var title = '';
+                var value = '百度一下';
+                if (_count) {
+                    value = value + '(' + _count + ')'
+                    title = '已为您清除了' + _count + '条广告';
+                }
+                btn.attr('title', title).val(value);
+            }
+            return _count;
+        };
+    };
 
     function clearBaiduAd() {
         return $("#content_left div[data-click] span:contains('广告')")
@@ -35,12 +44,20 @@
 
     $(document).bind("DOMNodeInserted", function (e) {
         var length = clearBaiduAd();
-        showClearCount(length);
+        counter.addCount(length);
+        counter.showCount();
+
+        // 1s之后将计数计重置为0
+        delayer.setDelay(function () {
+            counter.clear();
+        }, 1000);
     });
 
+    // 页面加载完成后，点击“百度一下”重新计数
     $(function () {
         $('[type="submit"]').click(function () {
-            $(this).data('count', 0);
+            counter.clear();
+            counter.showCount();
         });
     });
 })();
