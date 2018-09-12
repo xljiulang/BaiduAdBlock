57bd6f7130a9629b2af028bb59a74e2f9e29a2c0
xljiulang <366193849@qq.com>
2018-09-12 11:38:29 +0800

    创建项目

---
+++ scripts/include.js
@@ -0,0 +1,7 @@
+﻿; (function () {
+    $(document).bind("DOMNodeInserted", function (e) {
+        $("#content_left div[data-click] span:contains('广告')")
+            .parents("#content_left div[data-click]")
+            .remove();
+    });
+})();
